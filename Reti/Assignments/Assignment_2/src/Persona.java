bbc6756a2cc4d17e186cfc0e1423008344200b37
Simone <passe98@gmail.com>
2021-10-03 10:34:50 +0200

    Update assignment_2

--- Reti/Assignments/Assignment_2/src/Persona.java
+++ Reti/Assignments/Assignment_2/src/Persona.java
@@ -1,14 +1,29 @@
-public class Persona implements Runnable{
-    public final int numero;
+import java.util.concurrent.ThreadLocalRandom;
 
-    // Costruttore della classe Persona.
-    // @param numero il numero della numeratrice
+public class Persona implements Runnable{
+    private final int numero;
+    private final int minDelay = 0;
+    private final int maxDelay = 1000;
+
+    // Costruttore.
+    // @param numero: il numero della numeratrice.
     public Persona(int numero) {
         this.numero = numero;
     }
 
     // Metodo principale.
+    @Override
     public void run() {
+        System.out.printf("Persona con il num %d: sono allo sportello\n", numero);
 
+        // Aspetto un intervallo di tempo compreso fra minDelay e maxDelay ms (inclusi).
+        int delay = ThreadLocalRandom.current().nextInt(minDelay, maxDelay + 1);
+        try {
+            Thread.sleep(delay);
+        } catch(InterruptedException e) {
+            System.err.println("Interruzione su sleep.");
+        }
+
+        System.out.printf("Persona con il num %d: esco dall'ufficio\n", numero);
     }
 }
--- Reti/Assignments/Assignment_2/src/Ufficio.java
+++ Reti/Assignments/Assignment_2/src/Ufficio.java
@@ -1,19 +1,27 @@
+/*
+ * @author Simone Passera
+ */
+
 import java.util.concurrent.*;
 
 public class Ufficio {
     public static final int numSportelli = 4;
-    public static final int dimSala = 10;
+    public static final int dimSala2 = 8;
 
     public static void main(String[] args) {
         // Leggo il numero di persone da riga di comando.
         if(args.length != 1) {
-            System.err.println("Esegui come: java Ufficio <numero_persone>");
+            System.err.println("Usage: java Ufficio <numero_persone>");
             System.exit(1);
         }
+
         int numPersone = Integer.parseInt(args[0]);
 
-        // Creo una coda bloccante per memorizzare i task (persone) in arrivo.
-        BlockingQueue<Runnable> sala = new LinkedBlockingDeque<Runnable>(dimSala);
+        // Creo una coda bloccante per memorizzare i task (persone) in attesa nella sala1.
+        BlockingQueue<Runnable> sala1 = new LinkedBlockingDeque<Runnable>();
+
+        // Creo una coda bloccante per memorizzare i task (persone) in attesa nella sala2.
+        BlockingQueue<Runnable> sala2 = new ArrayBlockingQueue<Runnable>(dimSala2);
 
         // Creo un pool di thread con al massimo 'numSportelli' thread.
         ExecutorService sportelli = new ThreadPoolExecutor(
@@ -21,20 +29,36 @@ public class Ufficio {
                 numSportelli, // Numero massimo di thread possibili nel pool.
                 0L, // Tempo di keep-alive per i thread.
                 TimeUnit.SECONDS,
-                sala, // Coda bloccante per i task.
-                new ThreadPoolExecutor.AbortPolicy() // Politica di rifiuto di default
-                // che solleva una RejectedExecutionException.
+                sala2, // Coda bloccante per i task.
+                new RejectedExecutionHandler() {
+                    @Override
+                    public void rejectedExecution(Runnable persona, ThreadPoolExecutor sportelli) {
+                        try {
+                            sportelli.getQueue().put(persona);
+                        } catch(InterruptedException e) {
+                            System.err.println("Interruzione put (sala2).");
+                        }
+                    }
+                } // Politica di rifiuto: la persona rifiutata in sala2 aspetta che sia libera
         );
 
-        // Creazione dei task.
-        for(int i = 0; i < numPersone; i++) {
+        // Creazione dei task (persone).
+        for(int i = 1; i <= numPersone; i++) {
+            sala1.add(new Persona(i));
+        }
+
+        // Sposto le persone dalla sala1 in sala2.
+        for(int i = 1; i <= numPersone; i++) {
             try {
-                sportelli.execute(new Persona(i));
-            }
-            catch(RejectedExecutionException e) {
-                System.err.printf("Viaggiatore %d: sala esaurita\n", i);
+                sportelli.execute(sala1.take());
+            } catch(RejectedExecutionException e) {
+                System.err.printf("Persona %d: sala2 esaurita\n", i);
+            } catch(InterruptedException e) {
+                e.printStackTrace();
             }
         }
+
+        sportelli.shutdown(); // Chiudo gli sportelli, dopo che la sala2 è vuota
     }
 }
 
