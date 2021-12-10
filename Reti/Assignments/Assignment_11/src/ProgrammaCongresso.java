799d008517f270f48493c479708ceea5c34cd57e
Simone <passe98@gmail.com>
2021-12-10 17:55:53 +0100

    Update Assignment_11

--- Reti/Assignments/Assignment_11/src/ProgrammaCongresso.java
+++ Reti/Assignments/Assignment_11/src/ProgrammaCongresso.java
@@ -1,10 +1,14 @@
 import java.rmi.*;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.*;
+import java.util.concurrent.locks.*;
 
 public class ProgrammaCongresso extends UnicastRemoteObject implements ProgrammaCongressoInterface {
 
     HashMap<String, List<String>> day1, day2, day3;
+    ReadWriteLock locks = new ReentrantReadWriteLock();
+    Lock readLock = locks.readLock();
+    Lock writeLock = locks.writeLock();
 
     public ProgrammaCongresso() throws RemoteException {
         super();
@@ -21,45 +25,56 @@ public class ProgrammaCongresso extends UnicastRemoteObject implements Programma
     }
 
     public HashMap<String, List<String>> program(int day) throws RemoteException {
-        switch (day) {
-            case 1:
-                return day1;
-            case 2:
-                return day2;
-            case 3:
-                return day3;
-            default:
-                return null;
+        readLock.lock();
+
+        try {
+            switch (day) {
+                case 1:
+                    return day1;
+                case 2:
+                    return day2;
+                case 3:
+                    return day3;
+                default:
+                    return null;
+            }
+        } finally {
+            readLock.unlock();
         }
     }
 
-    public synchronized int register(String name, int day, int session) throws RemoteException {
+    public int register(String name, int day, int session) throws RemoteException {
+        writeLock.lock();
 
-        if (day < 1 || day > 3) return 1;
-        if (session < 1 || session > 12) return 2;
+        try {
+            if (day < 1 || day > 3) return 1;
+            if (session < 1 || session > 12) return 2;
 
-        HashMap<String, List<String>> dayRequest = null;
+            HashMap<String, List<String>> dayRequest = null;
 
-        switch (day) {
-            case 1:
-                dayRequest = day1;
-                break;
-            case 2:
-                dayRequest = day2;
-                break;
-            case 3:
-                dayRequest = day3;
+            switch (day) {
+                case 1:
+                    dayRequest = day1;
+                    break;
+                case 2:
+                    dayRequest = day2;
+                    break;
+                case 3:
+                    dayRequest = day3;
+            }
+
+            List<String> interventi = dayRequest.get("S" + session);
+
+            if (interventi.size() == 5) return 3;
+
+            if (interventi.contains(name)) return 4;
+
+            interventi.add(name);
+            dayRequest.put("S" + session, interventi);
+
+            return 0;
+        } finally {
+            writeLock.unlock();
         }
-
-        List<String> interventi = dayRequest.get("S" + session);
-
-        if (interventi.size() == 5) return 3;
-
-        if (interventi.contains(name)) return 4;
-
-        interventi.add(name);
-        dayRequest.put("S" + session, interventi);
-
-        return 0;
     }
 }
