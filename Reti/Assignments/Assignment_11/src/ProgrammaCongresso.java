0f017b3522acc0866843f03c20bc02c55985cfce
Simone <passe98@gmail.com>
2021-12-10 21:15:09 +0100

    Update Assignment_11

--- Reti/Assignments/Assignment_11/src/ProgrammaCongresso.java
+++ Reti/Assignments/Assignment_11/src/ProgrammaCongresso.java
@@ -6,9 +6,16 @@ import java.util.concurrent.locks.*;
 public class ProgrammaCongresso extends UnicastRemoteObject implements ProgrammaCongressoInterface {
 
     HashMap<String, List<String>> day1, day2, day3;
-    ReadWriteLock locks = new ReentrantReadWriteLock();
-    Lock readLock = locks.readLock();
-    Lock writeLock = locks.writeLock();
+    ReadWriteLock locksDay1;
+    Lock readLockDay1;
+    Lock writeLockDay1;
+    ReadWriteLock locksDay2;
+    Lock readLockDay2;
+    Lock writeLockDay2;
+    ReadWriteLock locksDay3;
+    Lock readLockDay3;
+    Lock writeLockDay3;
+
 
     public ProgrammaCongresso() throws RemoteException {
         super();
@@ -22,59 +29,69 @@ public class ProgrammaCongresso extends UnicastRemoteObject implements Programma
             day2.put("S" + i, new ArrayList<>(5));
             day3.put("S" + i, new ArrayList<>(5));
         }
+
+        locksDay1 = new ReentrantReadWriteLock();
+        readLockDay1 = locksDay1.readLock();
+        writeLockDay1 = locksDay1.writeLock();
+        locksDay2 = new ReentrantReadWriteLock();
+        readLockDay2 = locksDay2.readLock();
+        writeLockDay2 = locksDay2.writeLock();
+        locksDay3 = new ReentrantReadWriteLock();
+        readLockDay3 = locksDay3.readLock();
+        writeLockDay3 = locksDay3.writeLock();
     }
 
     public HashMap<String, List<String>> program(int day) throws RemoteException {
-        readLock.lock();
-
-        try {
-            switch (day) {
-                case 1:
-                    return day1;
-                case 2:
-                    return day2;
-                case 3:
-                    return day3;
-                default:
-                    return null;
-            }
-        } finally {
-            readLock.unlock();
+        switch (day) {
+            case 1:
+                readLockDay1.lock();
+                try { return day1; } finally { readLockDay1.unlock(); }
+            case 2:
+                readLockDay2.lock();
+                try { return day2; } finally { readLockDay2.unlock(); }
+            case 3:
+                readLockDay3.lock();
+                try { return day3; } finally { readLockDay3.unlock(); }
+            default:
+                return null;
         }
     }
 
     public int register(String name, int day, int session) throws RemoteException {
-        writeLock.lock();
+        if (day < 1 || day > 3) return 1;
+        if (session < 1 || session > 12) return 2;
+
+        HashMap<String, List<String>> dayRequest = null;
+        Lock lock = null;
+
+        switch (day) {
+            case 1:
+                dayRequest = day1;
+                lock = writeLockDay1;
+                break;
+            case 2:
+                dayRequest = day2;
+                lock = writeLockDay2;
+                break;
+            case 3:
+                dayRequest = day3;
+                lock = writeLockDay3;
+        }
+
+        lock.lock();
 
         try {
-            if (day < 1 || day > 3) return 1;
-            if (session < 1 || session > 12) return 2;
-
-            HashMap<String, List<String>> dayRequest = null;
-
-            switch (day) {
-                case 1:
-                    dayRequest = day1;
-                    break;
-                case 2:
-                    dayRequest = day2;
-                    break;
-                case 3:
-                    dayRequest = day3;
-            }
-
             List<String> interventi = dayRequest.get("S" + session);
 
             if (interventi.size() == 5) return 3;
-
             if (interventi.contains(name)) return 4;
 
             interventi.add(name);
             dayRequest.put("S" + session, interventi);
-
-            return 0;
         } finally {
-            writeLock.unlock();
+            lock.unlock();
         }
+
+        return 0;
     }
 }
