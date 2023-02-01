6dfa1d87f5d456c5236848c77014f556978b231c
Simone <passe98@gmail.com>
2023-02-01 16:02:26 +0100

    Update Dadi

--- Scraping/Dadi/main.py
+++ Scraping/Dadi/main.py
@@ -1,22 +1,20 @@
+from collections import defaultdict
 from itertools import product
 
 
-def get_matching_event(event_condition, generic_outcome_space):
-    return set([sum_key for sum_key in generic_outcome_space if event_condition(sum_key)])
+def get_matching_event(event_condition, generic_sample_space):
+    return set([sum_key for sum_key in generic_sample_space.keys() if event_condition(sum_key)])
 
 
 def compute_event_probability(event_condition, generic_sample_space):
-    samples = get_matching_event(event_condition, generic_sample_space)
-
-    outcomes_num = 0
-    for dices_sum in samples:
-        outcomes_num += generic_sample_space[dices_sum]
+    event = get_matching_event(event_condition, generic_sample_space)
+    event_size = sum(generic_sample_space[dices_sum] for dices_sum in event)
 
     sample_space_num = 0
     for dices_sum in generic_sample_space:
         sample_space_num += generic_sample_space[dices_sum]
 
-    return outcomes_num / sample_space_num
+    return event_size / sum(generic_sample_space.values())
 
 
 def has_sum_of_21(dices_sum): return dices_sum == 21
@@ -25,15 +23,11 @@ def has_sum_of_21(dices_sum): return dices_sum == 21
 possible_rolls = list(range(1, 7))
 sample_space_tuples = set(product(possible_rolls, repeat=6))
 
-sample_space = dict()
+weighted_sample_space = defaultdict(int)
+
 for outcome in sample_space_tuples:
-    key = sum(outcome)
-    value = sample_space.get(key)
+    total = sum(outcome)
+    weighted_sample_space[total] += 1
 
-    if value is None:
-        sample_space.update({key: 1})
-    else:
-        sample_space.update({key: value + 1})
-
-prob = compute_event_probability(has_sum_of_21, sample_space)
+prob = compute_event_probability(has_sum_of_21, weighted_sample_space)
 print(f"6 rolls sum to 21 with a probability of {prob}")
